f5ae4ec8e2a239bdb492e286bbb13cdef6a727a3
Kirillzur <kirillzurav@gmail.com>
2025-10-18 19:55:58 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from '@chakra-ui/react'
+
+
+function App() {
+  
+  return (
+    <>
+      <h2>kdosakda</h2>
+      <Button variant='solid'>Click me</Button>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { Provider } from "@/components/ui/provider"
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import React from "react"
+
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+)
